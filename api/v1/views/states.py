b8f1a78eb04aa7f5d7ae9aa491517d9ccce0f620
Lesajay <craigmazwi@gmail.com>
2023-05-01 17:09:17 +0000

    api tasks

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,43 +8,39 @@ from flask import Blueprint, jsonify, request, abort
 from api.v1.views import app_views
 from models import storage
 from models.state import State
-from models.city import City
 
 
-@app_views.route('/states/<string:state_id>/cities', methods=['GET', 'POST'],
-                 strict_slashes=False)
-def cities(state_id):
-    """Create a new view for City objects that handles all default
+@app_views.route('/states', methods=['GET', 'POST'], strict_slashes=False)
+def states():
+    """Create a new view for State objects that handles all default
     RestFul API actions.
     """
-    state = storage.get('State', state_id)
-    if state is None:
-        abort(404)
     if request.method == 'GET':
-        return jsonify([val.to_dict() for val in state.cities])
+        return jsonify([val.to_dict() for val in storage.all('State')
+                        .values()])
     elif request.method == 'POST':
         post = request.get_json()
         if post is None or type(post) != dict:
             return jsonify({'error': 'Not a JSON'}), 400
         elif post.get('name') is None:
             return jsonify({'error': 'Missing name'}), 400
-        new_state = City(state_id=state_id, **post)
+        new_state = State(**post)
         new_state.save()
         return jsonify(new_state.to_dict()), 201
 
 
-@app_views.route('/cities/<string:city_id>',
+@app_views.route('/states/<string:state_id>',
                  methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
-def get_city_id(city_id):
-    """Retrieves a city object with a specific id"""
-    city = storage.get('City', city_id)
-    if city is None:
+def get_state_id(state_id):
+    """Retrieves a state object with a specific id"""
+    state = storage.get('State', state_id)
+    if state is None:
         abort(404)
     elif request.method == 'GET':
-        return jsonify(city.to_dict())
+        return jsonify(state.to_dict())
     elif request.method == 'DELETE':
-        city = storage.get('City', city_id)
-        storage.delete(city)
+        state = storage.get('State', state_id)
+        storage.delete(state)
         storage.save()
         return jsonify({}), 200
     elif request.method == 'PUT':
@@ -53,6 +49,6 @@ def get_city_id(city_id):
             return jsonify({'error': 'Not a JSON'}), 400
         for key, value in put.items():
             if key not in ['id', 'created_at', 'updated_at']:
-                setattr(city, key, value)
+                setattr(state, key, value)
                 storage.save()
-        return jsonify(city.to_dict()), 200
+        return jsonify(state.to_dict()), 200
--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -7,40 +7,42 @@
 from flask import Blueprint, jsonify, request, abort
 from api.v1.views import app_views
 from models import storage
-from models.amenity import Amenity
+from models.user import User
 
 
-@app_views.route('/amenities', methods=['GET', 'POST'], strict_slashes=False)
-def amenities():
-    """Create a new view for Amenity objects that handles all default
+@app_views.route('/users', methods=['GET', 'POST'], strict_slashes=False)
+def users():
+    """Create a new view for User objects that handles all default
     RestFul API actions.
     """
     if request.method == 'GET':
-        return jsonify([val.to_dict() for val in storage.all('Amenity')
+        return jsonify([val.to_dict() for val in storage.all('User')
                         .values()])
     elif request.method == 'POST':
         post = request.get_json()
         if post is None or type(post) != dict:
             return jsonify({'error': 'Not a JSON'}), 400
-        elif post.get('name') is None:
-            return jsonify({'error': 'Missing name'}), 400
-        new_amenity = Amenity(**post)
-        new_amenity.save()
-        return jsonify(new_amenity.to_dict()), 201
+        elif post.get('email') is None:
+            return jsonify({'error': 'Missing email'}), 400
+        elif post.get('password') is None:
+            return jsonify({'error': 'Missing password'}), 400
+        new_user = User(**post)
+        new_user.save()
+        return jsonify(new_user.to_dict()), 201
 
 
-@app_views.route('/amenities/<string:amenity_id>',
+@app_views.route('/users/<string:user_id>',
                  methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
-def get_amenity_id(amenity_id):
-    """Retrieves a amenity object with a specific id"""
-    amenity = storage.get('Amenity', amenity_id)
-    if amenity is None:
+def get_user_id(user_id):
+    """Retrieves a user object with a specific id"""
+    user = storage.get('User', user_id)
+    if user is None:
         abort(404)
     elif request.method == 'GET':
-        return jsonify(amenity.to_dict())
+        return jsonify(user.to_dict())
     elif request.method == 'DELETE':
-        amenity = storage.get('Amenity', amenity_id)
-        storage.delete(amenity)
+        user = storage.get('User', user_id)
+        storage.delete(user)
         storage.save()
         return jsonify({}), 200
     elif request.method == 'PUT':
@@ -49,6 +51,6 @@ def get_amenity_id(amenity_id):
             return jsonify({'error': 'Not a JSON'}), 400
         for key, value in put.items():
             if key not in ['id', 'created_at', 'updated_at']:
-                setattr(amenity, key, value)
+                setattr(user, key, value)
                 storage.save()
-        return jsonify(amenity.to_dict()), 200
+        return jsonify(user.to_dict()), 200
